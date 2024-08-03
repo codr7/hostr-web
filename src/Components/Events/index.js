81ed915211027bb5034bbdd133b6e4488187f2dd
Andreas Wild <codr7@protonmail.com>
2024-08-03 23:50:05 +0200

    Fts.

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,49 +1,99 @@
 import { AppConfig, formatDateTime, useAuth } from '../../app.js';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
-import { startTransition, Suspense, useCallback, useEffect, useState } from 'react';
-import Loader from '../Loader';
+import { Button, Stack, Table, TableBody, TableCell, TableHead, TableRow, TextField } from '@mui/material';
+import { Search, SkipNext, SkipPrevious } from '@mui/icons-material';
+import { useState } from 'react';
 
 export default function Component() {
     useAuth();
 
-    const { pageIndex, setPageIndex } = useState(0);
-    const { pageSize, setPageSize } = useState(10);
-    const [data, setData] = useState();
+    const [pageIndex, setPageIndex] = useState(0);
+    const [pageSize, setPageSize] = useState(20);
+    const [data, setData] = useState([]);
+    const [isSearching, setIsSearching] = useState(false);
 
-    useEffect(() => {
-        const refresh = async () => {
+    const [resultStart, setResultStart] = useState();
+    const [resultEnd, setResultEnd] = useState();
+
+    const headerStyle = {
+        fontSize: '110%',
+        textTransform: 'uppercase',
+        color: 'silver'
+    };
+
+    const dataStyle = {
+        fontSize: '100%'
+    };
+
+    const refresh = async () => {
+        setIsSearching(true);
+
+        try {
             const d = await (
                 await fetch(`${AppConfig.apiPath}/events?rowOffset=${pageIndex * pageSize}&rowLimit=${pageSize}`,
                     { mode: 'cors', headers: { 'Accept': 'application/json' } })
             ).json();
 
-            startTransition(() => setData(d));
-        };
+            setResultStart(pageSize * pageIndex);
+            setResultEnd(pageSize * pageIndex + d.length);
+            setData(d);
+        } finally {
+            setIsSearching(false);
+        }
+    };
 
+    const onSearch = () => {
+        setPageIndex(0);
         refresh();
-    }, [pageIndex, pageSize]);
+    };
+
+    const onNext = () => {
+        setPageIndex(pageIndex + 1);
+        refresh();
+    };
+
+    const onPrev = () => {
+        setPageIndex(pageIndex - 1);
+        refresh();
+    };
 
     return (
-        (data ? <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell width="120">Posted</TableCell>
-                    <TableCell>Type</TableCell>
-                    <TableCell>Key</TableCell>
-                    <TableCell>Data</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                    {data.map((ev, i) => {
-                        console.log(ev);
-                        return (<TableRow>
-                            <TableCell>{formatDateTime(new Date(ev.postedAt))}</TableCell>
-                            <TableCell>{ev.type}</TableCell>
-                            <TableCell>{ev.key && JSON.stringify(ev.key, null, 2)}</TableCell>
-                            <TableCell>{ev.data && JSON.stringify(ev.data, null, 2)}</TableCell>
-                        </TableRow>)
-                    })}
-            </TableBody>
-        </Table > : <Loader/>)
+        <Stack>
+            <div style={{ marginLeft: 20, marginTop: 20 }}>
+                <TextField style={{ width: 100 }} label="Page Size" value={pageSize} onChange={(e) => {
+                    const v = parseInt(e.target.value);
+                    setPageSize(v ? v : "");
+                    setIsSearching(false);
+                }} />
+                <Button style={{ verticalAlign: 'bottom' }} onClick={onSearch} disabled={isSearching}><Search /></Button>
+            </div>
+            <div>
+                {(data.length > 0) && <div style={{ marginLeft: 'auto', marginRight: 'auto', width: 200 }}>
+                    <Button style={{ verticalAlign: 'bottom' }} onClick={onPrev} disabled={isSearching}><SkipPrevious /></Button>
+                    <span>{resultStart} to {resultEnd}</span>
+                    <Button style={{ verticalAlign: 'bottom' }} onClick={onNext} disabled={isSearching}><SkipNext /></Button>
+                </div>}
+
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell style={headerStyle} width="130">Posted</TableCell>
+                            <TableCell style={headerStyle}>Type</TableCell>
+                            <TableCell style={headerStyle} width="200">Key</TableCell>
+                            <TableCell style={headerStyle}>Data</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((ev, i) => {
+                            return (<TableRow>
+                                <TableCell style={dataStyle}>{formatDateTime(new Date(ev.postedAt))}</TableCell>
+                                <TableCell style={dataStyle}>{ev.type}</TableCell>
+                                <TableCell style={dataStyle}>{ev.key && JSON.stringify(ev.key, null, 2)}</TableCell>
+                                <TableCell style={dataStyle}>{ev.data && JSON.stringify(ev.data, null, 2)}</TableCell>
+                            </TableRow>)
+                        })}
+                    </TableBody>
+                </Table>
+            </div>
+        </Stack>
     );
 }
