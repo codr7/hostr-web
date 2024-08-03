d423559d3f3a983393e18d4a2027facc40f89100
Andreas Wild <codr7@protonmail.com>
2024-08-03 20:30:16 +0200

    Fixes for events view.

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,10 +1,49 @@
-//import './style.css';
-import { useAuth } from '../../app.js';
+import { AppConfig, formatDateTime, useAuth } from '../../app.js';
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { startTransition, Suspense, useCallback, useEffect, useState } from 'react';
+import Loader from '../Loader';
 
 export default function Component() {
     useAuth();
-    
+
+    const { pageIndex, setPageIndex } = useState(0);
+    const { pageSize, setPageSize } = useState(10);
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const refresh = async () => {
+            const d = await (
+                await fetch(`${AppConfig.apiPath}/events?rowOffset=${pageIndex * pageSize}&rowLimit=${pageSize}`,
+                    { mode: 'cors', headers: { 'Accept': 'application/json' } })
+            ).json();
+
+            startTransition(() => setData(d));
+        };
+
+        refresh();
+    }, [pageIndex, pageSize]);
+
     return (
-        <div></div>
+        (data ? <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell width="120">Posted</TableCell>
+                    <TableCell>Type</TableCell>
+                    <TableCell>Key</TableCell>
+                    <TableCell>Data</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                    {data.map((ev, i) => {
+                        console.log(ev);
+                        return (<TableRow>
+                            <TableCell>{formatDateTime(new Date(ev.postedAt))}</TableCell>
+                            <TableCell>{ev.type}</TableCell>
+                            <TableCell>{ev.key && JSON.stringify(ev.key, null, 2)}</TableCell>
+                            <TableCell>{ev.data && JSON.stringify(ev.data, null, 2)}</TableCell>
+                        </TableRow>)
+                    })}
+            </TableBody>
+        </Table > : <Loader/>)
     );
 }
--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ export const AppConfig = {apiPath: 'http://localhost:5000/api/v1'}
 
 export const AppContext = React.createContext();
 
+export const DateTimeFormat = new Intl.DateTimeFormat('sv-SE', {dateStyle: 'short', timeStyle: 'short'});
+
+export const formatDateTime = (value) => {
+    return DateTimeFormat.format(value);
+};
+
 export const useAuth = () => {
     const { appCx } = useContext(AppContext);
     const navigate = useNavigate();
