447e5d353a1a242e9649116d55aa8e51f0950780
Andreas Wild <codr7@protonmail.com>
2024-08-09 21:01:19 +0200

    Add interval selection.

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-import { AppConfig, AppContext, formatDateTime, useAuth } from '../../app.js';
-import { Button, Stack, Table, TableBody, TableContainer, TableHead, TableRow } from '@mui/material';
+import { AppConfig, AppContext, formatDay, formatTime, useAuth } from '../../app.js';
+import { Button, MenuItem, Stack, Table, TableBody, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import { Search } from '@mui/icons-material';
 import { DateTimePicker } from '@mui/x-date-pickers';
@@ -11,14 +11,15 @@ export default function Component() {
     useAuth();
 
     const now = new Date();
-    var d = new Date(now.getUTCFullYear(), now.getMonth(), now.getDay());
-    var iv = 24 * 60;
+    var startDate = new Date(now.getUTCFullYear(), now.getMonth(), now.getDate());
+    var endDate = new Date(startDate.getTime() + 24 * 60 * 60 * 1000);
 
     const [data, setData] = useState(null);
     const [isSearching, setIsSearching] = useState(false);
-    const [startAt, setStartAt] = useState(dayjs(d));
-    const [endAt, setEndAt] = useState(dayjs(new Date(d.getTime() + iv * 60 * 1000)));
-    const [interval, setInterval] = useState(iv);
+    const [startAt, setStartAt] = useState(dayjs(startDate));
+    const [endAt, setEndAt] = useState(dayjs(endDate));
+    const [interval, setInterval] = useState(24 * 60);
+    const [searchInterval, setSearchInterval] = useState(24 * 60);
 
     const headerStyle = {
         fontSize: '110%',
@@ -46,35 +47,60 @@ export default function Component() {
             ).json();
 
             setData(d);
+            setSearchInterval(interval);
         } finally {
             setIsSearching(false);
         }
     };
 
+    const formatHeader = it => {
+        let result;
+
+        switch (searchInterval) {
+            case DAYS:
+                result = formatDay(it);
+                break;
+            case HOURS:
+                result = formatTime(it);
+                break;
+            default:
+                throw new Error(`Invalid header: ${it}`)
+        }
+
+        return result;
+    }
+
     const onSearch = async () => await refresh();
+
+    const DAYS = 24 * 60;
+    const HOURS = 60;
 
     return (
         <Stack>
             <Stack direction='row' spacing={2} style={{ marginLeft: 20, marginTop: 20 }}>
                 <DateTimePicker label='Start at' value={startAt} onChange={setStartAt} />
                 <DateTimePicker label='End at' value={endAt} onChange={setEndAt} />
+                <TextField
+                    select
+                    value={interval}
+                    label='Interval'
+                    onChange={e => setInterval(e.target.value)}>
+                    <MenuItem value={DAYS}>Days</MenuItem>
+                    <MenuItem value={HOURS}>Hours</MenuItem>
+                </TextField>
                 <Button variant='outlined' style={{ verticalAlign: 'bottom' }} startIcon={<Search />} onClick={onSearch} disabled={isSearching || interval === ""}>Search</Button>
             </Stack>
             {data && <TableContainer><Table sx={{ [`& .${tableCellClasses.root}`]: { borderBottom: "none" } }}>
                 <TableHead>
                     <TableRow>
                         <TableCell style={headerStyle}>Pool</TableCell>
-                        {data.intervals.map((it) => (<TableCell style={headerStyle}>
-                            {formatDateTime(new Date(it))}
-                        </TableCell>))}
+                        {data.intervals.map((it) => (<TableCell style={headerStyle}>{formatHeader(it)}</TableCell>))}
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {data.calendars.filter(cal => !cal.pool.hasInfiniteCapacity).map(cal => (<TableRow>
                         <TableCell>{cal.pool.name}</TableCell>
-                        {cal.capacity.map(cap => (<TableCell style={dataStyle}>
-                            {parseInt(cap.total) - parseInt(cap.used)}
-                        </TableCell>))}
+                        {cal.capacity.map(cap => (<TableCell style={dataStyle}>{parseInt(cap.total) - parseInt(cap.used)}</TableCell>))}
                     </TableRow>)
                     )}
                 </TableBody>
--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { useContext, useEffect } from 'react';
 import {useNavigate} from 'react-router-dom';
+import dayjs from 'dayjs';
 
 export const AppConfig = {apiPath: 'http://localhost:5000/api/v1'}
 
 export const AppContext = React.createContext();
 
-export const DateTimeFormat = new Intl.DateTimeFormat('sv-SE', {dateStyle: 'short', timeStyle: 'short'});
+export const DateFormat = 'YY-MM-DD';
+export const DayFormat = 'dd DD';
+
+export const TimeFormat = 'HH:mm';
+
+export const formatDay = (value) => {
+    return dayjs(value).format(DayFormat);
+};
+
+export const formatTime = (value) => {
+    return dayjs(value).format(TimeFormat);
+};
 
 export const formatDateTime = (value) => {
-    return DateTimeFormat.format(value);
+    return dayjs(value).format(`${DateFormat} ${TimeFormat}`);
 };
 
 export const useAuth = () => {
